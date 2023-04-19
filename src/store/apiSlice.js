e56cedc1c0a7f3ee71aaa521d5ac0e5d1e0739a8
Tim Gaines <tim@timdoes.com>
2023-04-18 19:35:28 -0700

    lesson 04

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -2,5 +2,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
-    endpoints: (builder) => ({}),
+    endpoints: (builder) => ({
+        getServices: builder.query({
+            query: () => '/services',
+        })
+    }),
 });
+
+export const { useGetServicesQuery } = api;
