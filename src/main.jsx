7d4d3c1f614b99b12043cd1bb2350ee258e57d14
Tim Gaines <tim@timdoes.com>
2023-04-19 21:31:50 -0700

    lesson 22

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import App from "./App";
 import "./index.css";
+import { api } from "./store/apiSlice";
 
 if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
@@ -12,6 +13,10 @@ if (import.meta.env.DEV) {
     onUnhandledRequest: "bypass",
   });
 }
+
+store.dispatch(api.endpoints.getDogs.initiate());
+store.dispatch(api.endpoints.getServices.initiate());
+store.dispatch(api.endpoints.getService.initiate('a098239'));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
